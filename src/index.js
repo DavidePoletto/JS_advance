e44f5158e439a1fc2afd0422a6ae5da150f95b52
Davide Poletto <153924428+DavidePoletto@users.noreply.github.com>
2024-03-02 02:12:43 +0100

    update day2

--- src/index.js
+++ src/index.js
@@ -9,31 +9,51 @@ backgroundImg.src = 'assets/IMG/background2.png';
 backgroundImg.alt = 'background image';
 container.appendChild(backgroundImg);
 
-
-/*//header
-const header = document.createElement('section');
-header.className = 'header';
-container.appendChild(header);
-
-//header title
-const titleBox = document.createElement('section');
-titleBox.className = 'title_box';
-header.appendChild(titleBox);
-
-const title = document.createElement('h1');
-title.id = 'title';
-title.textContent = 'libri';
-titleBox.appendChild(title);*/
-
 //main content
 const mainContent = document.createElement('section');
 mainContent.className = 'main_content';
 container.appendChild(mainContent);
 
-const title = document.createElement('h1');
+const dropdownContainer = document.createElement('div');
+dropdownContainer.classList.add('dropdown-container');
+mainContent.appendChild(dropdownContainer);
+
+const dropdownButton = document.createElement('button');
+dropdownButton.classList.add('dropdown-btn');
+dropdownButton.innerHTML = `
+    <span class="line"></span>
+    <span class="line"></span>
+    <span class="line"></span>
+`;
+dropdownContainer.appendChild(dropdownButton);
+
+const dropdownMenu = document.createElement('div');
+dropdownMenu.classList.add('dropdown-menu');
+dropdownContainer.appendChild(dropdownMenu);
+
+// Creazione di un array di voci di menu
+const menuItems = ['Home', 'About', 'Services', 'Contact'];
+
+// Iterazione sull'array e creazione di un elemento di menu per ogni voce
+menuItems.forEach(item => {
+    const menuItem = document.createElement('div');
+    menuItem.textContent = item;
+    dropdownMenu.appendChild(menuItem);
+});
+
+dropdownButton.addEventListener('click', function() {
+    dropdownMenu.classList.toggle('show');
+    console.log('Clicked on hamburger menu');
+});
+
+const titleBox = document.createElement('div');
+titleBox.className = 'title_box'
+mainContent.appendChild(titleBox);
+
+const title = document.createElement('img');
 title.id = 'title_blur';
-title.textContent = 'FIND YOUR BOOK';
-mainContent.appendChild(title);
+title.src = './assets/IMG/logo4.png'
+titleBox.appendChild(title);
 
 const searchBar = document.createElement('input');
 searchBar.type = 'text';
