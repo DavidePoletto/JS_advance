2dec258cc56eca36f4f84cd23bf6fb9f9ad48a80
Davide Poletto <153924428+DavidePoletto@users.noreply.github.com>
2024-04-09 19:52:06 +0200

    loadmorebutton

--- src/index.js
+++ src/index.js
@@ -87,5 +87,56 @@ function searchBooks(event) {
     }
 }
 
+function loadMoreBooks() {
+    const resultsContainer = document.getElementById('results');
+    const lastBook = resultsContainer.lastElementChild;
+    const lastBookOffset = lastBook.offsetTop + lastBook.clientHeight;
+    const pageOffset = window.scrollY + window.innerHeight;
+    const bottomOffset = 20;
+
+    if (pageOffset > lastBookOffset - bottomOffset) {
+        // Effettua un'altra richiesta per caricare ulteriori libri...
+        const searchQuery = document.getElementById('search_input').value;
+        const apiUrl = `https://openlibrary.org/search.json?q=${searchQuery}&limit=4&offset=${resultsContainer.children.length}`;
+
+        $.ajax({
+            url: apiUrl,
+            method: 'GET',
+            success: function(response) {
+                const books = response.docs;
+                books.forEach(book => {
+                    // Crea e aggiungi elementi per visualizzare i libri...
+                    const bookDiv = document.createElement('div');
+                    bookDiv.classList.add('book');
+
+                    const titleElement = document.createElement('h2');
+                    titleElement.textContent = book.title;
+                    bookDiv.appendChild(titleElement);
+
+                    const authorElement = document.createElement('p');
+                    authorElement.textContent = `Author: ${book.author_name ? book.author_name.join(', ') : 'Unknown'}`;
+                    bookDiv.appendChild(authorElement);
+
+                    if (book.cover_i) {
+                        const coverUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+                        const coverImg = document.createElement('img');
+                        coverImg.src = coverUrl;
+                        bookDiv.appendChild(coverImg);
+                    }
+
+                    // Aggiungi il libro al container dei risultati
+                    resultsContainer.appendChild(bookDiv);
+                });
+            },
+            error: function(error) {
+                console.log("Si è verificato un errore:", error);
+            }
+        });
+    }
+}
+
 // Aggiungi il listener per l'evento "keypress" all'input di ricerca
 document.getElementById('search_input').addEventListener('keypress', searchBooks);
+
+
+document.getElementById('loadMoreBtn').addEventListener('click', loadMoreBooks);
