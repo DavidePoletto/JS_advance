133abc073fe0de5b77b4cbb6c5403a39277d193b
Davide Poletto <153924428+DavidePoletto@users.noreply.github.com>
2024-04-22 21:37:12 +0200

    webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+  mode: 'development',
+  entry: './src/assets/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
+  module: { rules: [
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.js$/i,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }
+  ]},
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'applicazione',
+      template: './src/index.html'
+    })
+  ]
+
 };
