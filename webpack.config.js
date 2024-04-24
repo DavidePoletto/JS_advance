84e0f7ebaf81e82d9e16749602298809ab1eb204
Davide Poletto <153924428+DavidePoletto@users.noreply.github.com>
2024-04-24 19:24:24 +0200

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/assets/js/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   module: { rules: [
     {
@@ -22,11 +23,23 @@ module.exports = {
           presets: ['@babel/preset-env']
         }
       }
+    },
+    {
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'assets/IMG',
+            esModule: false
+          }
+        }
+      ]
     }
   ]},
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'applicazione',
       template: './src/index.html'
     })
   ]
